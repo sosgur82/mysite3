8884193c8b40020c383bf0a6fe4ac78ba45a217a
BIT <BIT@DESKTOP-K00OCLP>
2017-09-18 16:25:08 +0900

    init commit

---
+++ src/main/java/com/bigdata2017/mysite/aspect/MeasureExecutionTimeAspect.java
@@ -0,0 +1,35 @@
+package com.bigdata2017.mysite.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class MeasureExecutionTimeAspect {
+	@Around("execution(* *..repository.*.*(..)) ||execution(* *..service.*.*(..)) || execution(* *..controller.*.*(..))")
+	public Object aroundAdvice(ProceedingJoinPoint pjp) 
+		throws Throwable{
+		// before advice code	
+		StopWatch stropWatch = new StopWatch();
+		stropWatch.start();
+		
+		Object result = pjp.proceed();
+		
+		//after advice code 
+
+		stropWatch.stop();
+		Long totalTime = stropWatch.getTotalTimeMillis();
+		
+		
+		String className = pjp.getTarget().getClass().getName();
+		String methodName = pjp.getSignature().getName();
+		String taskName = className + "." + methodName;
+		
+		System.out.println("[ExecutionTime]" + "[" + taskName + "]" + totalTime + "mills");
+				
+		return result;
+	}
+}
